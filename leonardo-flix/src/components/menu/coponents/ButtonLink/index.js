438f34f08fd01ffe5d2455a6846770c8dcf1ad40
Leonardo Carrera da Silva <leonardosilva.ads@hotmail.com>
2020-07-27 13:22:23 -0300

    Commit incial do projeto

---
+++ leonardo-flix/src/components/menu/coponents/ButtonLink/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+function ButtonLink(props) {
+
+
+    return (
+        
+            <a classN ame={props.className} href={props.href}>
+            Novo video 
+            </a>
+            
+    );
+}
+
+export default ButtonLink;
