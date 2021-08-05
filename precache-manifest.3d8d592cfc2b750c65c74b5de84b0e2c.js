3fed2751e56b26cf98ab74be9b23e082bab0e9d2
Leonardo Carrera da Silva <59487403+LeonardoCarreraSilva@users.noreply.github.com>
2021-08-05 12:35:46 -0300

    Updates

---
+++ precache-manifest.3d8d592cfc2b750c65c74b5de84b0e2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e496f148942e6fbae8c9e606044051",
+    "url": "/leonardo-flix/index.html"
+  },
+  {
+    "revision": "5337fdc1f0aa4da7802e",
+    "url": "/leonardo-flix/static/css/main.7fced621.chunk.css"
+  },
+  {
+    "revision": "2d1908370f36b48774ba",
+    "url": "/leonardo-flix/static/js/2.efff6d48.chunk.js"
+  },
+  {
+    "revision": "c87e50d81cc7b5311525cc6fd5482ea5",
+    "url": "/leonardo-flix/static/js/2.efff6d48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5337fdc1f0aa4da7802e",
+    "url": "/leonardo-flix/static/js/main.28d5413a.chunk.js"
+  },
+  {
+    "revision": "506caeb5c84a47d96409",
+    "url": "/leonardo-flix/static/js/runtime-main.ace497fa.js"
+  },
+  {
+    "revision": "ad2fd3a427c7e020b3bfa71fb18bcf9c",
+    "url": "/leonardo-flix/static/media/Logo.ad2fd3a4.png"
+  }
+]);
